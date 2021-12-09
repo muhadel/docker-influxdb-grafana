83af61614632becb568fa11f38e837626ff06412
mohamed adel <01mohamedadel@gmail.com>
2021-12-10 01:09:13 +0200

    debug changes

--- disk_usage_cronjob/src/utils/influxdb_v1.py
+++ disk_usage_cronjob/src/utils/influxdb_v1.py
@@ -1,10 +1,13 @@
-from influxdb_client import InfluxDBClient, BucketRetentionRules
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb import InfluxDBClient
 from utils.config import config
 
 
 class InfluxDB:
     __influx_client = None
+    host = config.get('INFLUXDB_CONTAINER_HOST')
+    port = config.get('INFLUXDB_CONTAINER_PORT')
+    username = config.get('INFLUXDB_USERNAME')
+    password = config.get('INFLUXDB_PASSWORD')
     url = config.get('INFLUXDB_URL')
     token = config.get('INFLUXDB_ADMIN_TOKEN')
     org = config.get('INFLUXDB_ORG')
@@ -20,34 +23,29 @@ class InfluxDB:
         :return: InfluxDB client instance, return the same client instance when creating a new InfluxDB instance.
         """
         if InfluxDB.__influx_client is None:
+
             print('[*] Creating new influx client instance')
             print("[*]", InfluxDB.url, InfluxDB.token, InfluxDB.org)
-            InfluxDB.__influx_client = InfluxDBClient(url=InfluxDB.url, token=InfluxDB.token,org=InfluxDB.org)
+            InfluxDB.__influx_client = InfluxDBClient(host=InfluxDB.host, port=InfluxDB.port, username=InfluxDB.username, password=InfluxDB.password)
             print("CLient", InfluxDB.__influx_client)
             # buckets_api = InfluxDB.__influx_client.buckets_api()
             # bucket = buckets_api.find_bucket_by_name("ADEL_BUCKET")
             # print("bucket===>", bucket)
 
         # Created bucket if not exists
-        # InfluxDB.create_bucket(InfluxDB, InfluxDB.bucket)
+        InfluxDB.create_bucket(InfluxDB, InfluxDB.bucket)
 
         return InfluxDB.__influx_client
 
     def create_bucket(self, bucket_name):
         print('[*****] Try to create bucket', bucket_name)
-        buckets_api = self.__influx_client.buckets_api()
-        # print('[*] Create this bucket', bucket_name)
-        # bucket = buckets_api.find_bucket_by_name(bucket_name)
-        # print('bucket_found', bucket)
-        # if not bucket:
-        retention_rules = BucketRetentionRules(type="expire", every_seconds=360000)
-        created_bucket = buckets_api.create_bucket(bucket_name=bucket_name,
-                                            bucket=bucket_name,
-                                            retention_rules=retention_rules,
-                                            org=InfluxDB.org,
-                                            org_id=InfluxDB.org)
-        print('created_bucket', created_bucket)
-        print('[*] Bucket [' + bucket_name + '] Created successfully')
+        buckets = self.__influx_client.get_list_database()
+        if bucket_name not in buckets:
+            self.__influx_client.create_database(bucket_name)
+            print('[*] Bucket [' + bucket_name + '] Created successfully')
+
+            
+        
 
     def write_data(self, measurement, fieldSet, tagSet=None):
         """
@@ -70,28 +68,27 @@ class InfluxDB:
         Line protocol with many fields : weather temperature=82,waveHeight=2.1
         ----------------------------------------------------------------------------------------------------------------
         """
-        # define influxDB write endpoint to write data
-        write_api = self.__influx_client.write_api(write_options=SYNCHRONOUS)
+    
         # concatenate comma before tag set if tagSet parameter is defined
         tagSet = ',' + tagSet if tagSet is not None else ''
         data = "{}{} {}".format(measurement, tagSet, fieldSet)
-        write_api.write(self.bucket, self.org, data)
+        self.__influx_client.write(data=data, protocol='line')
         print("[*] Writing data to [" + self.bucket + "] Bucket")
         print("[*] Data Saved: " + data + "\n\n")
         
 
-    def execute_query(self):
-        """
-        Execute Query on Influx DB
-        This method returns all data in all measurements by specifying bucket name in the query (Bucket_Name=default)
-        :return: None
-        """
-        query = 'from(bucket: "default") |> range(start: -1h)'
-        tables = self.__influx_client.query_api().query(query, org=self.org)
-        print(tables)
-        for table in tables:
-            for record in table.records:
-                print(record)
+    # def execute_query(self):
+    #     """
+    #     Execute Query on Influx DB
+    #     This method returns all data in all measurements by specifying bucket name in the query (Bucket_Name=default)
+    #     :return: None
+    #     """
+    #     query = 'from(bucket: "default") |> range(start: -1h)'
+    #     tables = self.__influx_client.query_api().query(query, org=self.org)
+    #     print(tables)
+    #     for table in tables:
+    #         for record in table.records:
+    #             print(record)
 
     def close_connection(self):
         """
