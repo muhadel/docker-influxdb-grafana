a26d9cefca8ca7de2128664bfce35851014c0677
mohamed adel <01mohamedadel@gmail.com>
2021-12-10 15:15:03 +0200

    debug changes

--- k8s/deploy-resources.sh
+++ k8s/deploy-resources.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 pwd
-kubectl apply -f ./deployments/grafana-deployment.yaml
-kubectl apply -f ./services/grafana-service.yaml
-kubectl apply -f ./ingress/ingress.yaml
+KUB_NAME_SPACE=docker-influxdb-grafana
+kubectl delete all --all -n $KUB_NAME_SPACE
+kubectl delete namespace $KUB_NAME_SPACE
+kubectl create ns $KUB_NAME_SPACE
+kubectl apply -f deployments/grafana-deployment.yaml
+kubectl apply -f services/grafana-service.yaml
+kubectl apply -f ingress/ingress.yaml
