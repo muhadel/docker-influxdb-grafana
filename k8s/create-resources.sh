a4eee195d12683bc3efdce0c2c2bac21eef02586
mohamed adel <01mohamedadel@gmail.com>
2021-12-11 00:54:19 +0200

    debug changes

--- k8s/create-resources.sh
+++ k8s/create-resources.sh
@@ -16,7 +16,7 @@ kubectl apply -f k8s/deployments/influxdb-deployment.yaml
 kubectl apply -f k8s/services/influxdb-service.yaml
 echo "Creating disk_usage_cronjob Resources..."
 kubectl apply -f k8s/deployments/disk_usage_cronjob-deployment.yaml
-kubectl apply -f k8s/services/disk_usage_cronjob-service.yaml
+# kubectl apply -f k8s/services/disk_usage_cronjob-service.yaml
 # Ingress
 echo "Creating Ingress service..."
 kubectl apply -f k8s/ingress/ingress.yaml
