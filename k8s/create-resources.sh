b618e058e1dc1af7a6477cfb5183912c16a0b7c3
mohamed adel <01mohamedadel@gmail.com>
2021-12-10 23:36:30 +0200

    debug changes

--- k8s/create-resources.sh
+++ k8s/create-resources.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+echo "Deleting Namespace..."
 KUB_NAME_SPACE=app
 kubectl delete all --all -n $KUB_NAME_SPACE
 kubectl delete namespace $KUB_NAME_SPACE
+echo "Creating New Namespace..."
 kubectl create ns $KUB_NAME_SPACE
 # Grafana
+echo "Creating Grafana Resources..."
 kubectl apply -f k8s/deployments/grafana-deployment.yaml
 kubectl apply -f k8s/services/grafana-service.yaml
 # Influxdb
-kubectl apply -f k8s/deployments/influxdb-deployment.yaml
+echo "Creating Influxdb Resources..."
 kubectl apply -f k8s/configmap/env-influxdb-configmap.yaml
+kubectl apply -f k8s/deployments/influxdb-deployment.yaml
 kubectl apply -f k8s/services/influxdb-service.yaml
 # Ingress
+echo "Creating Ingress service..."
 kubectl apply -f k8s/ingress/ingress.yaml
