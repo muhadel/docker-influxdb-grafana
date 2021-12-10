2d28176c9f400fe8a49378606013b46eb0f0b917
mohamed adel <01mohamedadel@gmail.com>
2021-12-10 15:09:53 +0200

    debug changes

--- k8s/deploy-resources.sh
+++ k8s/deploy-resources.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-kubectl create ns docker-influxdb-grafana
 kubectl apply -f ./deployments/grafana-deployment.yaml
 kubectl apply -f ./services/grafana-service.yaml
 kubectl apply -f ./ingress/ingress.yaml
