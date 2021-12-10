ac21f598ce6f5c9a05e271d3a78e878034a4d9c3
mohamed adel <01mohamedadel@gmail.com>
2021-12-10 17:00:25 +0200

    debug changes

--- k8s/deploy-resources.sh
+++ k8s/deploy-resources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
-KUB_NAME_SPACE=docker-influxdb-grafana
+KUB_NAME_SPACE=myapp
 kubectl delete all --all -n $KUB_NAME_SPACE
 kubectl delete namespace $KUB_NAME_SPACE
 kubectl create ns $KUB_NAME_SPACE
--- k8s/flush-namespace.sh
+++ k8s/flush-namespace.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-KUB_NAME_SPACE=docker-influxdb-grafana
+KUB_NAME_SPACE=myapp
 kubectl delete all --all -n $KUB_NAME_SPACE
 kubectl delete namespace $KUB_NAME_SPACE
+kubectl create ns $KUB_NAME_SPACE
