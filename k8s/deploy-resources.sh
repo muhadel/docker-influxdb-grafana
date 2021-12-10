72d20fa1c38832ae97ba8a14cf9289178a9f8783
mohamed adel <01mohamedadel@gmail.com>
2021-12-10 22:15:09 +0200

    debug changes

--- k8s/deploy-resources.sh
+++ k8s/deploy-resources.sh
@@ -3,6 +3,11 @@ KUB_NAME_SPACE=app
 kubectl delete all --all -n $KUB_NAME_SPACE
 kubectl delete namespace $KUB_NAME_SPACE
 kubectl create ns $KUB_NAME_SPACE
+# Grafana
 kubectl apply -f k8s/deployments/grafana-deployment.yaml
 kubectl apply -f k8s/services/grafana-service.yaml
+# Influxdb
+# kubectl apply -f k8s/deployments/influxdb-deployment.yaml
+# kubectl apply -f k8s/services/influxdb-service.yaml
+# Ingress
 kubectl apply -f k8s/ingress/ingress.yaml
