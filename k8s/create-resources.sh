8efc6f217d6d5e2843070eff611d51dda7fac701
mohamed adel <01mohamedadel@gmail.com>
2021-12-11 22:11:04 +0200

    debug changes

--- k8s/create-resources.sh
+++ k8s/create-resources.sh
@@ -18,13 +18,13 @@ kubectl apply -f k8s/services/influxdb-service.yaml
 echo "Creating cronjob config maps..."
 # wait for influxdb pod creattion to take it's IP address and add it in the configMap file
 sleep 4
-#x
+#
 # Get INFLUXDB pod Ip address and remove white spaces
 INFLUXDB_POD_IP=$(kubectl exec influxdb-pod -n app -- hostname -I | xargs)
 cat k8s/configmap/env-cronjob-configmap.yaml | sed "s/{{INFLUXDB_POD_IP}}/$INFLUXDB_POD_IP/g" | kubectl apply -f -
 echo "------------------------------------------------"
 echo "Creating disk_usage_cronjob Resources..."
-xkubectl apply -f k8s/pods/cronjob-pod.yaml
+kubectl apply -f k8s/pods/cronjob-pod.yaml
 kubectl apply -f k8s/services/cronjob-service.yaml
 echo "------------------------------------------------"
 # Ingress
