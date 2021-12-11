73033c7bf6d63ba42c93f49f442213a61b8fba8b
mohamed adel <01mohamedadel@gmail.com>
2021-12-11 16:09:18 +0200

    debug changes

--- k8s/create-resources.sh
+++ k8s/create-resources.sh
@@ -5,7 +5,7 @@ kubectl delete all --all -n $KUB_NAME_SPACE
 kubectl delete namespace $KUB_NAME_SPACE
 echo "Creating New Namespace..."
 kubectl create ns $KUB_NAME_SPACE
-# Config Maps
+# Create Influxdb config maps
 kubectl apply -f k8s/configmap/env-influxdb-configmap.yaml
 # Grafana
 echo "Creating Grafana Resources..."
@@ -16,8 +16,12 @@ echo "Creating Influxdb Resources..."
 # kubectl apply -f k8s/deployments/influxdb-deployment.yaml
 kubectl apply -f k8s/pods/influxdb-pod.yaml
 kubectl apply -f k8s/services/influxdb-service.yaml
+echo "Creating cronjob config maps..."
+INFLUXDB_POD_IP=kubectl exec influxdb-pod -n app -- hostname -I
+echo "This is the InfluxDB IP" + $INFLUXDB_POD_IP
+cat k8s/configmap/env-cronjob-configmap.yaml | sed "s/{{INFLUXDB_POD_IP}}/$INFLUXDB_POD_IP/g" | kubectl apply -f -
+echo "------------------------------------------------"
 echo "Creating disk_usage_cronjob Resources..."
-# kubectl apply -f k8s/configmap/env-cronjob-configmap.yaml
 # kubectl apply -f k8s/deployments/cronjob-deployment.yaml
 kubectl apply -f k8s/pods/cronjob-pod.yaml
 kubectl apply -f k8s/services/cronjob-service.yaml
