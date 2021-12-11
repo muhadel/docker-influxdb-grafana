856cb403f41a9958ccb4f6ca1a79c153d6de6f29
mohamed adel <01mohamedadel@gmail.com>
2021-12-11 20:58:11 +0200

    debug changes

--- disk_usage_cronjob/src/utils/config.py
+++ disk_usage_cronjob/src/utils/config.py
@@ -2,5 +2,5 @@ from dotenv import dotenv_values
 import os
 
 config = dotenv_values()  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
-# print('[*] Config', config)
-# print('[*] OS Config', os.getenv("INFLUXDB_CONTAINER_HOST"))
+print('[*] Config', config)
+print('[*] OS Config', os.getenv("INFLUXDB_CONTAINER_HOST"))
